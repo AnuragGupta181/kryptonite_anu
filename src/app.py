132e5c516cad4dca4098d4329ef9aa183d5e235a
vansh Sharma <vanshsharma7832@gmail.com>
2026-02-07 18:38:26 +0530

    added get_high_region and fixed requirements.txt

--- src/app.py
+++ src/app.py
@@ -5,6 +5,7 @@ load_dotenv()
 from src.routes.home_router import router as HomeRouter
 from src.routes.map_router import router as MapRouter
 from src.routes.fireYolo_router import router as FireYoloRouter
+from src.routes.getHighRegionArea_router import router as GetHighRegionAreaRouter
 app=FastAPI(title="Fire Detector",description="Kryptonite hackathon")
 
 
@@ -19,3 +20,4 @@ app.add_middleware(
 app.include_router(HomeRouter,prefix="/api/user")
 app.include_router(MapRouter,prefix="/api/map")
 app.include_router(FireYoloRouter,prefix="/api/fireYolo")
+app.include_router(GetHighRegionAreaRouter,prefix="/api/get_high_region_area")
---
+++ src/routes/getHighRegionArea_router.py
@@ -0,0 +1,29 @@
+import fastapi
+router=fastapi.APIRouter()
+from src.pipeline.data_fetcher_pipeline import Data_Fetcher_Pipeline
+from src.exception import MyException
+import pandas as pd
+import json
+
+import sys
+import logging
+@router.post("/get_hight_regions_area")
+async def high_regions(
+    country: str = "india",
+    state: str = "up",
+    day_range: int = 3
+):
+    try:
+            pipeline=Data_Fetcher_Pipeline(country=country,state=state,day_range=day_range)
+            data_ingestion_artifact=await pipeline.start_data_ingestion()
+            data_transformation_artifact=await pipeline.start_data_transformation(data_ingestion_artifact)
+
+            data=pd.read_csv(data_transformation_artifact.feature_store_path)
+            data=data[data['confidence']=='h']
+            data=json.dumps(data.to_dict())
+            
+            return {"data": data}
+   
+    except Exception as e:
+        logging.error(f"Error in map_locations: {str(e)}")
+        raise MyException(e, sys)
